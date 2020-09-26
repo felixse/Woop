2dc5cf55a108da011892d5425e313a02da4bae5b
Felix Seidl <f.seidl@me.com>
2020-09-26 20:55:10 +0200

    fix input.text computed property

--- Woop/Assets/WoopAdapter.js
+++ Woop/Assets/WoopAdapter.js
@@ -1,15 +1,25 @@
 ﻿function woopAdapter(properties, methods) {
     var input = {
-        text: properties.selection && properties.selection != "" ? properties.selection : properties.fullText,
         fullText: properties.fullText,
         selection: properties.selection,
         isSelection: properties.isSelection,
         postInfo: methods.postInfo,
         postError: methods.postError,
-        insert: methods.insert
+        insert: methods.insert,
+
+        get text() {
+            return this.isSelection ? this.selection : this.fullText;
+        },
+
+        set text(value) {
+            if (this.isSelection) {
+                this.selection = value;
+            } else {
+                this.fullText = value;
+            }
+        }
     };
     main(input);
 
-    // todo this breaks if a script is using fullText or selection instead of text
     return input.text;
 }
